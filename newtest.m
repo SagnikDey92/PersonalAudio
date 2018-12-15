ac62e71674f13db2a07d73a32165b4f7898e2ca9
EmperorGlitch <mail.sagnik.dey@gmail.com>
2018-12-15 16:48:40 +0530

    Added Todo and minor parameter changes in code

--- newtest.m
+++ newtest.m
@@ -1,17 +1,17 @@
 [x,Fs] = audioread('audioshort.wav');
 x = x(:, 1);
-x = x(1:16000, 1); 
+x = x(1:160000, 1); %cutting audio short
 c = 340;                    % Sound velocity (m/s)
 fs = 16000;                 % Sample frequency (samples/s)
-r = [1 2.5 3];              % Receiver position [x y z] (m)
-s = [2 4.5 5];              % Source position [x y z] (m)
-L = [5 5 5];              % Room dimensions [x y z] (m)
+r = [9 9 9];                % Receiver position [x y z] (m)
+s = [9 7 4];                % Source position [x y z] (m)
+L = [10 10 10];             % Room dimensions [x y z] (m)
 beta = 0.4;                 % Reverberation time (s)
 n = 512;                    % Number of samples
 SNR = 100;%high SNR
 h = rir_generator(c, fs, r, s, L, beta, n);
 h = h';
-hnoise=awgn(h, 100)-h;
+hnoise = awgn(h, 100)-h;
 sigp2 = 10*log10(norm(hnoise,2)^2/numel(hnoise));
 snr2 = sigp2-100;
 sigma2w = 10^(snr2/10);
@@ -24,13 +24,15 @@ snr1 = sigp1-SNR;
 sigma2v = 10^(snr1/10);
 y = ynoise;
 H=zeros(n, 1);
-epsilon=1e-10;%using very low epsilon
+epsilon=1e-13;%using very low epsilon
 index=1;
 Xsize=size(x, 1);
 Ru=epsilon*eye(n);
 threshlow = 1e-5;
+%sigma2v = 1e-13;
+%sigma2w = 1e-13;
 while index < Xsize - n
-    if(mod(index, 1600)==0)
+    if(mod(index, 1600)==0&&2==3)
         h = awgn(h, SNR);
         y = conv(x, h);
         y = awgn(y, SNR);
@@ -45,6 +47,6 @@ while index < Xsize - n
     Ru = (eye(n) - K*xf')*Rm;
     index=index+1;
     clc;
-    norm(h-flipud(H))/norm(h)
+    norm(h-flipud(H))/norm(h)*100
     index
 end   

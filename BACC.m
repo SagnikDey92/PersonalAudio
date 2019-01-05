8f888b42f346df71298d359be922313588e84584
Sudhanshu <sudhanshu.ktj@gmail.com>
2019-01-06 03:42:31 +0530

    Added verification of Bacc and adjusted position of control points and speakers

--- BACC.m
+++ BACC.m
@@ -4,10 +4,17 @@ x = x(:, 1);
 delta = 0.1;                % what to do? 
 x = x(1:160000, 1);         % cutting audio short
 M = 1024;                   % filter length
-speaker1 = [3 3 3];
+
+%fiddled around with speaker and control positions
+%this gives best possible output
+speaker1 = [6 2 3];
 speaker2 = [5 5 5];
-darkcentre = [1 2 3];
-brightcentre = [7 5 2];
+
+%this position gives best output
+darkcentre = [0.4001 2 3];%basically placing dark spot as far as away as possible 
+brightcentre = [6.4001 2 3];%placing bright nearer to the speaker
+
+
 %channel for control points below (1000 around dark zone and brightzone of volume 10x10x10 cm^3)
 [ dcontrol1, dcontrol2, bcontrol1, bcontrol2 ] = init_channels(darkcentre, brightcentre, speaker1, speaker2);
 K = 1000;
@@ -18,7 +25,24 @@ opt = pinv(Rd'*Rd + delta*eye(M*2, M*2))*(Rb'*Rb);
 filter1 = filterpred(1:M, 1);       %filter for speaker 1
 filter2 = filterpred(M+1:end, 1);   %filter for speaker 2
 
+centreindex=445;%index of original control point for both bright and dark
 
+%Applying filter to input signal
+u1=conv(x,filter1);
+u2=conv(x,filter2);
+
+%Applying channel to filtered input
+ybright=conv(bcontrol1(centreindex,:),u1)+conv(bcontrol2(centreindex,:),u2);
+ydark=conv(dcontrol1(centreindex,:),u1)+conv(dcontrol2(centreindex,:),u2);
+
+%plot of both output signals for comparison
+plot(ybright);
+hold on;
+plot(ydark);
+
+%norm of this difference represents the contrast between the 2 signals
+%higher norm higher contrast
+norm(ybright-ydark)
 %OLD CODE BELOW
 
 %{
